4e5d52bfac572b71f39a2a18775397366e42260f
Shiquana Johnson <56485181+crispyjr@users.noreply.github.com>
2022-06-28 01:44:47 -0400

    letter buttons work

--- hangman.js
+++ hangman.js
@@ -23,3 +23,33 @@
 //medical id
 //pos 1,2,3,4,5,6 and 8,9
 //basically if there is enough space, they both can be on top. 
+
+let tempword = "fruit";
+const wordSplit = tempword.split("");
+let position = document.getElementsByClassName("lineBox")[3];
+
+function showPosition(){
+    if(tempword.length=5){
+        for(let i=2 ;i<7;i++){
+            document.getElementsByClassName("lineBox")[i].style.visibility="visible";
+
+        }
+        //document.getElementsByClassName("lineBox")[3].style.visibility="visible";
+    }
+    
+}
+
+//for when u click on a letter button
+function touchLetterButton(x){
+    x.disabled = true;
+    checkLetter(x);
+    position.innerText += x.innerText;
+}
+
+//check if the letter is there
+function checkLetter(x){
+    if(x.innerText.toUpperCase() == wordSplit[0].toUpperCase()){
+        document.getElementsByClassName("lineBox")[2].innerText = wordSplit[0];
+    }
+    console.log(x.innerText.toUpperCase() == wordSplit[0].toUpperCase());
+}
